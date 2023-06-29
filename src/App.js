32641d1dda053b4a45ab0bcf6c34df4f210f9623
Kevin Reilly <kreilly253@gmail.com>
2023-06-28 21:02:03 -0700

    added video and track models to graphql

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+
 import "./App.css";
+
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage } from 'aws-amplify';
 import {
@@ -12,6 +14,7 @@ import {
   View,
   withAuthenticator,
 } from '@aws-amplify/ui-react';
+
 import { listNotes } from "./graphql/queries";
 import {
   createNote as createNoteMutation,
